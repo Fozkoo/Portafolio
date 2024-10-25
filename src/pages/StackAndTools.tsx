2569e9871cdee0de1ace4cb5a058a583a9e80385
Tiziano <tmartinelli@alumnos.unsada.edu.ar>
2024-10-25 19:19:49 -0300

    chore(create new page ) - [x] Create Stack & Tools section.
    - [x] Create first version view to images

--- src/pages/StackAndTools.tsx
+++ src/pages/StackAndTools.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import imageHelper from '../service/Helper';
+
+
+interface Image {
+  src: string;
+  alt: string;
+}
+
+interface ImageSectionProps {
+  images: Image[];
+}
+
+function ImageSection({ images }: ImageSectionProps) {
+  return (
+    <section className={`flex w-full flex-wrap gap-5 justify-between items-center`}>
+      {images.map((image, index) => (
+        <img key={index} src={image.src} className="h-28" alt={image.alt} />
+      ))}
+    </section>
+  );
+}
 
 function StackAndTools() {
   return (
-    <div>StackAndTools</div>
-  )
+    <div className="container-presentation  bg-slate-50 flex flex-col h-[90vh] justify-center gap-2 items-center mx-48">
+      
+
+      
+      
+      <div className="container-section flex flex-col w-full">
+        <div className="container-text">
+          <p className="font-bold text-7xl">Stack</p>
+        </div>
+        <div className="container-img flex flex-wrap rounded-3xl gap-5 mt-6 justify-between items-center p-7 bg-[#E8E8E8]">
+          <ImageSection images={imageHelper.stackImages.filter(img => img.group === "lenguajes")} />
+          <ImageSection images={imageHelper.stackImages.filter(img => img.group === "frameworks")} />
+        </div>
+      </div>
+
+      
+      <div className="container-section mt-10 flex flex-col w-full">
+        <div className="container-text">
+          <p className="font-bold text-7xl">Tools</p>
+        </div>
+        <div className="container-img flex flex-wrap rounded-3xl gap-5 mt-6 justify-between items-center p-7 bg-[#E8E8E8]">
+          <ImageSection images={imageHelper.toolsImages.filter(img => img.group === "tools")} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default StackAndTools
+export default StackAndTools;
---
+++ src/service/Helper.ts
@@ -0,0 +1,48 @@
+import img1 from "..//assets/html-edit-1.svg"
+import img2 from "..//assets/css-edit-1.svg"
+import img3 from "..//assets/java-edit-1.svg"
+import img4 from "..//assets/javascript-edit-1.svg"
+import img5 from "..//assets/typescript-edit-1.svg"
+import img6 from "..//assets/mysql-edit-1.svg"
+
+import img7 from "..//assets/react-edit-1.svg"
+import img8 from "..//assets/tailwindcss-edit-1.svg"
+import img9 from "..//assets/boostrap-edit-1.svg"
+import img10 from "..//assets/springboot-edit-1.svg"
+import img11 from "..//assets/ionic-edit-1.svg"
+
+import img12 from "..//assets/git-edit-1.svg"
+import img13 from "..//assets/github-edit-1.svg"
+import img14 from "..//assets/postman.svg"
+import img15 from "..//assets/trello.svg"
+import img16 from "..//assets/figma-icon.svg"
+import img17 from "..//assets/excalidraw-edit-1.svg"
+
+
+const stackImages = [
+    { src: img1, alt: "HTML", group: "lenguajes"},
+    { src: img2, alt: "CSS" , group: "lenguajes"},
+    { src: img3, alt: "Java" , group: "lenguajes"},
+    { src: img4, alt: "JavaScript" , group: "lenguajes"},
+    { src: img5, alt: "MySQL" , group: "lenguajes"},
+    { src: img6, alt: "TypeScript" , group: "lenguajes"},
+    { src: img7, alt: "React" , group: "frameworks"},
+    { src: img8, alt: "Tailwind CSS" , group: "frameworks"},
+    { src: img9, alt: "Bootstrap" , group: "frameworks"},
+    { src: img10, alt: "Spring Boot" , group: "frameworks"},
+    { src: img11, alt: "Ionic" , group: "frameworks"}
+  ];
+  
+  const toolsImages = [
+    { src: img12, alt: "Git" , group: "tools"},
+    { src: img13, alt: "GitHub" , group: "tools"},
+    { src: img14, alt: "Postman" , group: "tools"},
+    { src: img15, alt: "Trello" , group: "tools"},
+    { src: img16, alt: "Figma" , group: "tools"},
+    { src: img17, alt: "Excalidraw" , group: "tools"}
+  ];
+  
+
+
+export default { stackImages, toolsImages }
+
