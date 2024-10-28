ba965a43fde59e3711c4b4ed0dfe1708114379af
Tiziano <tmartinelli@alumnos.unsada.edu.ar>
2024-10-28 11:09:56 -0300

    chore(create proyects tarjet)

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from 'react';
 import Helper from '../service/Helper';
 
 const Carousel: React.FunctionComponent = () => {
-  const images = Helper.imagesArecobus; // Accede al array de imágenes
+  const images = Helper.imagesArecobus; 
   const [currentIndex, setCurrentIndex] = useState<number>(0);
 
-  // Cambia la imagen cada 3 segundos
+  
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 3000);
+    }, 5000);
 
     return () => clearInterval(interval);
   }, [images.length]);
@@ -19,12 +19,12 @@ const Carousel: React.FunctionComponent = () => {
   };
 
   return (
-    <div className="relative w-full z-[1000] overflow-hidden shadow-lg">
-      <div className="w-full h-64 ">
+    <div className="relative w-full z-[1000] overflow-hidden ">
+      <div className="w-full h-56 p-1">
         <img
           src={images[currentIndex].src} 
           alt={images[currentIndex].alt}
-          className="object-cover w-full  h-full transition-opacity duration-500 ease-in-out"
+          className="object-cover w-full h-full transition-opacity duration-700 ease-in-out"
         />
       </div>
       <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 flex space-x-2">
--- src/pages/Proyects.tsx
+++ src/pages/Proyects.tsx
@@ -1,5 +1,12 @@
-import React from 'react'
 import Carousel from '../components/Carousel'
+import helper from '../service/Helper'
+
+
+const img1 = helper.stackImages[6].src
+const img2 = helper.stackImages[7].src
+const img3 = helper.stackImages[9].src
+const img4 = helper.stackImages[5].src
+
 
 export const Proyects = () => {
   return (
@@ -15,15 +22,30 @@ export const Proyects = () => {
 
           <div className="container-img h-[600px] flex justify-center items-center rounded-3xl  mt-6  p-10 bg-[#E8E8E8]">
 
-            <div className="container-tarjets flex h-[256px] w-[70%] rounded-2xl ">
+            <div className="container-tarjets   flex flex-col overflow-hidden shadow-xl h-[550px] w-[350px]  rounded-xl">
 
-              <div className="section carousel rounded-s-2xl overflow-hidden  w-[60%]">
+              <div className="container-carousel flex  justify-center items-center h-[45%]  bg-white">
                 <Carousel />
               </div>
 
-              <div className="section-text bg-[#232121] rounded-e-2xl flex justify-center items-center h-[256px]  w-[40%]">
-                asdads
+              <div className="container-info-tarjets justify-center ite flex flex-col border-t-2 bg-white  p-3 h-[55%] ">
+                <p className="text-black text-2xl font-semibold">Arecobus</p>
+                <p className='text-gray-500'>Proyecto que recopila la informacion de los distintos buses que operan en San Antonio de Areco.</p>
+                <p className='text-black text-xl font-semibold'>Tecnologias utilizadas</p>
+
+                <div className="container-img flex w-full gap-3 h-[50px] justify-normal items-center ">
+                  <img src={img1} className='h-[35px]' />
+                  <img src={img2} className='h-[30px]'/>
+                  <img src={img3} className='h-[35px]'/>
+                  <img src={img4} className='h-[35px]'/>
+                </div>
+
+                <div className="container-button-view-more flex mt-3 w-full justify-center items-center ">
+                  <button className="bg-black text-white px-4 py-2 rounded-lg">Conoce mas!</button>
+                </div>
+
               </div>
+
             </div>
 
           </div>
